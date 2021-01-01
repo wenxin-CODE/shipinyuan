39ce6b04c30025c15a63dee6145b1cdc5b23192c
ablocker <oct-month@qq.com>
2021-01-01 15:05:06 +0800

    '合起来'

--- src/views/Brand.vue
+++ src/views/Brand.vue
@@ -1,363 +1,363 @@
 <template>
   <div>
-  <el-table
-    :data="tableData"
-    border
-    style="width: 100%">
-    <el-table-column
-      fixed
-      prop="id"
-      label="公司编号"
-      width="150">
-    </el-table-column>
-    <el-table-column
-      prop="from"
-      label="来源"
-      width="120">
-    </el-table-column>
-    <el-table-column
-      prop="state"
-      label="组织状态"
-      width="150">
-    </el-table-column>
-    <el-table-column
-      prop="color"
-      label="色泽"
-      width="120">
-    </el-table-column>
-    <el-table-column
-      prop="water"
-      label="水分"
-      width="120">
-    </el-table-column>
-    <el-table-column
-      prop="protein"
-      label="蛋白质"
-      width="120">
-    </el-table-column>
-    <el-table-column
-      prop="fat"
-      label="脂肪"
-      width="120">
-    </el-table-column>
-    <el-table-column
-      prop="suger"
-      label="乳糖"
-      width="120">
-    </el-table-column>
-    <el-table-column
-      prop="sucrose"
-      label="蔗糖"
-      width="120">
-    </el-table-column>
-    <el-table-column
-      prop="weiA"
-      label="维生素A"
-      width="120">
-    </el-table-column>
-    <el-table-column
-      prop="weiD"
-      label="维生素D"
-      width="120">
-    </el-table-column>
-    <el-table-column
-      prop="weiE"
-      label="维生素E"
-      width="120">
-    </el-table-column>
-    <el-table-column
-      prop="weiK1"
-      label="维生素K1"
-      width="120">
-    </el-table-column>
-    <el-table-column
-      prop="weiB1"
-      label="维生素B1"
-      width="120">
-    </el-table-column>
-    <el-table-column
-      prop="weiB2"
-      label="维生素B2"
-      width="120">
-    </el-table-column>
-    <el-table-column
-      prop="weiB6"
-      label="维生素B6"
-      width="120">
-    </el-table-column>
-    <el-table-column
-      prop="weiB12"
-      label="维生素B12"
-      width="120">
-    </el-table-column>
-    <el-table-column
-      prop="niacin"
-      label="烟酸"
-      width="120">
-    </el-table-column>
-    <el-table-column
-      prop="folic"
-      label="叶酸"
-      width="120">
-    </el-table-column>
-    <el-table-column
-      prop="panto_acid"
-      label="泛酸"
-      width="120">
-    </el-table-column>
-    <el-table-column
-      prop="weiC"
-      label="维生素C"
-      width="120">
-    </el-table-column>
-    <el-table-column
-      prop="biotin"
-      label="生物素"
-      width="120">
-    </el-table-column>
-    <el-table-column
-      prop="choline"
-      label="胆碱"
-      width="120">
-    </el-table-column>
-    <el-table-column
-      prop="Fe"
-      label="铁"
-      width="120">
-    </el-table-column>
-    <el-table-column
-      prop="Ca"
-      label="钙"
-      width="120">
-    </el-table-column>
-    <el-table-column
-      prop="Zn"
-      label="锌"
-      width="120">
-    </el-table-column>
-    <el-table-column
-      prop="P"
-      label="磷"
-      width="120">
-    </el-table-column>
-    <el-table-column
-      prop="Na"
-      label="钠"
-      width="120">
-    </el-table-column>
-    <el-table-column
-      prop="K"
-      label="钾"
-      width="120">
-    </el-table-column>
-    <el-table-column
-      prop="Mg"
-      label="镁"
-      width="120">
-    </el-table-column>
-    <el-table-column
-      prop="Cu"
-      label="铜"
-      width="120">
-    </el-table-column>
-    <el-table-column
-      prop="Mn"
-      label="锰"
-      width="120">
-    </el-table-column>
-    <el-table-column
-      prop="Se"
-      label="硒"
-      width="120">
-    </el-table-column>
-    <el-table-column
-      prop="I"
-      label="碘"
-      width="120">
-    </el-table-column>
-    <el-table-column
-      prop="Pb"
-      label="铅"
-      width="120">
-    </el-table-column>
-    <el-table-column
-      prop="Cr"
-      label="铬"
-      width="120">
-    </el-table-column>
-    <el-table-column
-      prop="Cd"
-      label="镉"
-      width="120">
-    </el-table-column>
-    <el-table-column
-      prop="As"
-      label="总砷"
-      width="120">
-    </el-table-column>
-    <el-table-column
-      prop="linoleic_acid"
-      label="亚油酸"
-      width="120">
-    </el-table-column>
-    <el-table-column
-      prop="α_linoleic_acid"
-      label="α-亚麻酸"
-      width="120">
-    </el-table-column>
-    <el-table-column
-      prop="α_bizhi"
-      label="亚油酸:α-亚麻酸"
-      width="120">
-    </el-table-column>
-    <el-table-column
-      prop="DHA"
-      label="DHA"
-      width="120">
-    </el-table-column>
-    <el-table-column
-      prop="ARA"
-      label="ARA"
-      width="120">
-    </el-table-column>
-    <el-table-column
-      prop="bili"
-      label="月桂酸和肉豆蔻酸占总脂肪酸的比例"
-      width="120">
-    </el-table-column>
-    <el-table-column
-      prop="bizhi"
-      label="反式脂肪酸与总脂肪酸的比值"
-      width="120">
-    </el-table-column>
-    <el-table-column
-      prop="OPO"
-      label="OPO"
-      width="120">
-    </el-table-column>
-    <el-table-column
-      prop="Cl"
-      label="氯"
-      width="120">
-    </el-table-column>
-    <el-table-column
-      prop="PH"
-      label="酸度"
-      width="120">
-    </el-table-column>
-    <el-table-column
-      prop="carbohydrate"
-      label="碳水化合物"
-      width="120">
-    </el-table-column>
-    <el-table-column
-      prop="ash_content"
-      label="灰分"
-      width="120">
-    </el-table-column>
-    <el-table-column
-      prop="Asp"
-      label="Asp"
-      width="120">
-    </el-table-column>
-    <el-table-column
-      prop="Thr"
-      label="Thr"
-      width="120">
-    </el-table-column>
-    <el-table-column
-      prop="Ser"
-      label="Ser"
-      width="120">
-    </el-table-column>
-    <el-table-column
-      prop="Glu"
-      label="Glu"
-      width="120">
-    </el-table-column>
-    <el-table-column
-      prop="Gly"
-      label="Gly"
-      width="120">
-    </el-table-column>
-    <el-table-column
-      prop="Ala"
-      label="Ala"
-      width="120">
-    </el-table-column>
-    <el-table-column
-      prop="Val"
-      label="Val"
-      width="120">
-    </el-table-column>
-    <el-table-column
-      prop="Met"
-      label="Met"
-      width="120">
-    </el-table-column>
-    <el-table-column
-      prop="Ile"
-      label="Ile"
-      width="120">
-    </el-table-column>
-    <el-table-column
-      prop="Leu"
-      label="Leu"
-      width="120">
-    </el-table-column>
-    <el-table-column
-      prop="Tyr"
-      label="Tyr"
-      width="120">
-    </el-table-column>
-    <el-table-column
-      prop="Phe"
-      label="Phe"
-      width="120">
-    </el-table-column>
-    <el-table-column
-      prop="Lys"
-      label="Lys"
-      width="120">
-    </el-table-column>
-    <el-table-column
-      prop="His"
-      label="His"
-      width="120">
-    </el-table-column>
-    <el-table-column
-      prop="Arg"
-      label="Arg"
-      width="120">
-    </el-table-column>
-    <el-table-column
-      prop="Pro"
-      label="Pro"
-      width="120">
-    </el-table-column>
-    <el-table-column
-      prop="amino_acid"
-      label="氨基酸"
-      width="120">
-    </el-table-column>
-    <el-table-column
-      fixed="right"
-      label="操作"
-      width="100">
-      <template slot-scope="scope">
-        <el-button @click="edit(scope.row)" type="text" size="small">修改</el-button>
-        <el-button @click="deletebrand(scope.row)" type="text" size="small">删除</el-button>
-      </template>
-    </el-table-column>
-  </el-table>
+    <el-table
+      :data="currentTabelData"
+      border
+      style="width: 100%">
+      <el-table-column
+        fixed
+        prop="id"
+        label="公司编号"
+        width="150">
+      </el-table-column>
+      <el-table-column
+        prop="地区"
+        label="来源"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        prop="组织状态"
+        label="组织状态"
+        width="150">
+      </el-table-column>
+      <el-table-column
+        prop="色泽"
+        label="色泽"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        prop="水分"
+        label="水分"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        prop="蛋白质"
+        label="蛋白质"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        prop="脂肪"
+        label="脂肪"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        prop="乳糖"
+        label="乳糖"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        prop="蔗糖"
+        label="蔗糖"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        prop="维生素A"
+        label="维生素A"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        prop="维生素D"
+        label="维生素D"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        prop="维生素E"
+        label="维生素E"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        prop="维生素K1"
+        label="维生素K1"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        prop="维生素B1"
+        label="维生素B1"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        prop="维生素B2"
+        label="维生素B2"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        prop="维生素B6"
+        label="维生素B6"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        prop="维生素B12"
+        label="维生素B12"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        prop="烟酸"
+        label="烟酸"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        prop="叶酸"
+        label="叶酸"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        prop="泛酸"
+        label="泛酸"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        prop="维生素C"
+        label="维生素C"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        prop="生物素"
+        label="生物素"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        prop="胆碱"
+        label="胆碱"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        prop="铁"
+        label="铁"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        prop="钙"
+        label="钙"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        prop="锌"
+        label="锌"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        prop="磷"
+        label="磷"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        prop="钠"
+        label="钠"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        prop="钾"
+        label="钾"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        prop="镁"
+        label="镁"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        prop="铜"
+        label="铜"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        prop="锰"
+        label="锰"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        prop="硒"
+        label="硒"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        prop="碘"
+        label="碘"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        prop="铅"
+        label="铅"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        prop="铬"
+        label="铬"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        prop="镉"
+        label="镉"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        prop="总砷"
+        label="总砷"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        prop="亚油酸"
+        label="亚油酸"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        prop="α-亚麻酸"
+        label="α-亚麻酸"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        prop="亚油酸:α-亚麻酸"
+        label="亚油酸:α-亚麻酸"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        prop="DHA"
+        label="DHA"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        prop="ARA"
+        label="ARA"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        prop="月桂酸和肉豆蔻酸占总脂肪酸的比例"
+        label="月桂酸和肉豆蔻酸占总脂肪酸的比例"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        prop="反式脂肪酸与总脂肪酸的比值"
+        label="反式脂肪酸与总脂肪酸的比值"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        prop="OPO"
+        label="OPO"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        prop="氯"
+        label="氯"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        prop="酸度"
+        label="酸度"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        prop="碳水化合物"
+        label="碳水化合物"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        prop="灰分"
+        label="灰分"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        prop="Asp"
+        label="Asp"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        prop="Thr"
+        label="Thr"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        prop="Ser"
+        label="Ser"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        prop="Glu"
+        label="Glu"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        prop="Gly"
+        label="Gly"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        prop="Ala"
+        label="Ala"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        prop="Val"
+        label="Val"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        prop="Met"
+        label="Met"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        prop="Ile"
+        label="Ile"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        prop="Leu"
+        label="Leu"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        prop="Tyr"
+        label="Tyr"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        prop="Phe"
+        label="Phe"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        prop="Lys"
+        label="Lys"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        prop="His"
+        label="His"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        prop="Arg"
+        label="Arg"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        prop="Pro"
+        label="Pro"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        prop="氨基酸总量"
+        label="氨基酸总量"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        fixed="right"
+        label="操作"
+        width="100">
+        <template slot-scope="scope">
+          <el-button @click="edit(scope.row)" type="text" size="small">修改</el-button>
+          <el-button @click="deletebrand(scope.row)" type="text" size="small">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
 
-  <el-pagination
-  background
-  layout="prev, pager, next"
-  :page-size="5"
-  :total="50"
-  @current-change="page">
-  </el-pagination>
+    <el-pagination
+      background
+      layout="prev, pager, next"
+      :page-size="page_size"
+      :total="tableData.length"
+      @current-change="changePage">
+    </el-pagination>
   </div>
 </template>
 
@@ -386,8 +386,10 @@
         })
         //row.id
       },
-      page(currentPage){
-        alert(currentPage)
+      changePage(currentPage) {
+        var start = this.page_size * (currentPage - 1)
+        var end = start + this.page_size
+        this.currentTabelData = this.tableData.slice(start, end)
       }
     },
 
@@ -408,6 +410,7 @@
           {
             that.tableData = res.data
             console.log(that.tableData);
+            that.changePage(1)
           }
         })
         .catch(error => {
@@ -419,58 +422,9 @@
       return {
         // total: null,
         // tableData: null,
-        tableData: [{
-          id: 1,
-          from: '陕西省', 
-          state: '干燥粉末，无结块，无正常视力可见外来异物',
-          color: '均匀一致的淡黄色',
-          water: 2.17,
-          protein: 24.9,
-          fat: 28.5,
-          suger: 30
-        }, {
-          id: 2,
-         state: '干燥粉末，无结块，无正常视力可见外来异物',
-          color: '均匀一致的淡黄色',
-          water: 2.17,
-          protein: 24.9,
-          fat: 28.5
-        }, {
-          id: 3,
-         state: '干燥粉末，无结块，无正常视力可见外来异物',
-          color: '均匀一致的淡黄色',
-          water: 2.17,
-          protein: 24.9,
-          fat: 28.5
-        }, {
-          id: 4,
-          state: '干燥粉末，无结块，无正常视力可见外来异物',
-          color: '均匀一致的淡黄色',
-          water: 2.17,
-          protein: 24.9,
-          fat: 28.5
-        }, {
-          id: 5,
-          state: '干燥粉末，无结块，无正常视力可见外来异物',
-          color: '均匀一致的淡黄色',
-          water: 2.17,
-          protein: 24.9,
-          fat: 28.5
-        }, {
-          id: 6,
-          state: '干燥粉末，无结块，无正常视力可见外来异物',
-          color: '均匀一致的淡黄色',
-          water: 2.17,
-          protein: 24.9,
-          fat: 28.5
-        }, {
-          id: 7,
-          state: '干燥粉末，无结块，无正常视力可见外来异物',
-          color: '均匀一致的淡黄色',
-          water: 2.17,
-          protein: 24.9, 
-          fat: 28.5
-        }]
+        tableData: [],
+        currentTabelData: [],
+        page_size: 5
       }
     }
   }
