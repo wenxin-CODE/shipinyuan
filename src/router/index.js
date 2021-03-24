a57d733220e8fbf97f7760a2ead503e4da444e9d
dly <2011429436@qq.com>
2021-03-24 16:31:32 +0800

    登陆注册

--- src/router/index.js
+++ src/router/index.js
@@ -14,6 +14,7 @@ import BrandFind from '../views/BrandFind'
 import InfoFind from '../views/InfoFind'
 import MilkFind from '../views/MilkFind'
 import Test from '../views/Test'
+import login from '../views/login'
 
 Vue.use(VueRouter)
 
@@ -115,6 +116,18 @@ const routes = [
         name: '产地判断',
         component: Test
       }]
+    },
+    {
+      path: "/login",
+      name: "login",
+      component:login
+      
+    }
+    ,{
+      path: "/register",
+      name: "register",
+      component: () =>
+        import("@/views/register.vue")
     }
   
 ]
@@ -124,5 +137,39 @@ const router = new VueRouter({
   base: process.env.BASE_URL,
   routes
 })
+
+router.beforeEach((to,from,next)=>
+{
+  //登录及注册页面可以直接进入,而主页面需要分情况
+  if(to.path=='/login')
+  {
+    next();
+    console.log(localStorage.s);
+  }
+  else if(to.path=='/register')
+  {
+    next();
+  }
+  else
+  {
+    if(from.path=="/login")//从登录页面可以直接通过登录进入主页面
+    {
+      next();
+    }
+    else{
+    	//从/进入,如果登录状态是true，则直接next进入主页面
+	      if(localStorage.s === "true")
+		    {
+		      next();
+		      console.log(localStorage['s'])
+		    }
+	    else {//如果登录状态是false，那么跳转至登录页面,需要登录才能进入主页面
+	      next('/login');
+	      console.log("需要登录")
+	    }
+    }
+  }
+})
+
 
 export default router
---
+++ src/views/register.vue
@@ -0,0 +1,124 @@
+<template>
+    <div id="background">
+      <div id="contain">
+        <h1>Register</h1>
+
+        <div class="form">
+          <label>用户名：</label><input type="text" v-model.trim="name"><br/>
+        </div>
+        <div class="form">
+          <label>密码：</label><input type="password" v-model.trim="password"><br/>
+        </div>
+        <div class="form">
+          <label>邮箱：</label><input type="email" v-model.trim="mail"><br/>
+        </div>
+        <div class="form">
+          <label>手机号：</label><input type="tel" v-model.trim="tel"><br/>
+        </div>
+        <button @click.prevent="handlefinish">提交</button>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'register',
+    props: {
+    msg: String
+  },
+  data(){
+    return{
+      name:"",
+      password:"",
+      mail:"",
+      tel:""
+  };
+  },methods:{
+  //点击完成按钮触发handlefinish
+    handlefinish:function()
+    {
+      if(localStorage['name']===this.name)
+      {
+        alert("用户名已存在");//如果用户名已存在则无法注册
+      }
+      else if(this.name==='')
+      {
+        alert("用户名不能为空");
+      }
+      else{//将新用户信息存储到localStorage
+        localStorage.setItem('name',this.name);
+        localStorage.setItem('password',this.password);
+        localStorage.setItem('mail',this.mail);
+        localStorage.setItem('tel',this.tel);
+        localStorage.setItem('s',"false");
+        alert("注册成功");
+        this.$router.replace('/Login');//完成注册后跳转至登录页面
+      }
+    }
+  }
+};
+</script>
+
+
+//css
+<style scoped>
+#background{
+  width: 100%;
+    height: 100%;
+    /* background: url("../assets/bg2.jpg"); */
+    background-size:100% 100%;
+    position: fixed;
+    top: 0;
+    left: 0;
+}
+#contain{
+  width: 580px;
+  height: 560px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%,-50%);
+  background:#00000090;
+  text-align: center;
+  border-radius: 20px;
+}
+#contain h1{
+  color: white;
+}
+.form{
+  color: white;
+  margin-left: 20%;
+  margin-top: 60px;
+  font-size: 20px;
+  text-align: left;
+}
+label{
+  float:left;
+  width: 5em;
+  margin-right: 1em;
+  text-align: right;
+}
+input,textarea{
+  margin-left: 10px;
+  padding: 4px;
+  border: solid 1px #4e5ef3;
+  outline: 0;
+  font: normal 13px/100% Verdana,Tahoma,sans-serif;
+  width: 200px;
+  height: 20px;
+  background:#f1f1f190;
+  box-shadow: rgba(0, 0, 0, 0.1) 0px 0px 8px;
+}
+input:hover,textarea:hover,input:focus,textarea:focus{border-color:#0d0aa1;}
+button{
+  position: relative;
+  height: 33px;
+  width: 150px;
+  background: rgba(35, 19, 252, 0.425);
+  border-radius: 10px;
+  margin-top: 38px;
+  box-shadow: none;
+  color: white;
+  margin-left: 40px;
+}
+</style>
