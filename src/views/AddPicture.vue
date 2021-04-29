795b78e4f49962e27cf8c14097ced161c6c7f435
dly <2011429436@qq.com>
2021-04-29 18:34:08 +0800

    图片

---
+++ src/views/AddPicture.vue
@@ -0,0 +1,84 @@
+<template>
+      <div class="uploadFile">
+        <!-- ul、li标签用于展示从本地添加的预览图片 -->
+        <ul v-show="uploadImg.length!=0">
+          <li v-for="(item,index) in uploadImg"
+              :key="index"
+              class="addPic">
+            <img :src="item">
+          </li>
+        </ul>
+        <input type="file"
+               id="file"
+               accept="image/*"
+               @change="getPicture($event)">
+        <button @click="callFile"
+                v-show="showInputImg">+</button>
+      </div>
+</template>
+<script>
+	export default {
+		data(){
+			return {
+				uploadImg:[]
+			}
+		},
+  		computed: {
+		    showInputImg () {
+		      return this.uploadImg.length < 3;
+		    }
+  		},
+  		methods: {
+  			getPicture (e) {
+		      //预览图片
+		      let src = window.URL.createObjectURL(e.target.files[0]);
+		      this.uploadImg.push(src);
+		
+		      //将图片文件转化成base64格式图片
+		      var reader = new FileReader();
+		      reader.onload = (e) => {
+		        //e.target.result  就是从本地读取的图片的base64格式,将它上传给服务器即可
+		        //使用axios的post方法上传即可
+		      }
+		      reader.readAsDataURL(e.target.files[0])
+		    },
+		    callFile () {
+		      //点击添加图片按钮，触发type:"file"的input标签
+		      let fileDom = document.querySelector("#file")
+		      fileDom.click()
+		    }
+  		}
+	}
+</script>
+<style scoped>
+		input {
+		  /* 隐藏默认的"打开文件"样式 */
+		  display: none;
+		}
+		.uploadFile {
+		  display: flex;
+		  height: 120px;
+		  line-height: 120px;
+		  padding: 10px 0;
+		  border-bottom: 1px solid rgb(235, 235, 235);
+		  overflow: hidden;
+		}
+		.uploadFile ul {
+		  display: flex;
+		}
+		.uploadFile ul li {
+		  margin-right: 10px;
+		}
+		.uploadFile .addPic img {
+		  height: 100px;
+		  width: 100px;
+		}
+		.uploadFile button {
+		  height: 100px;
+		  width: 100px;
+		  font-size: 50px;
+		  border: 1px dashed rgb(182, 182, 182);
+		  color: rgb(182, 182, 182);
+		  background-color: rgb(243, 243, 243);
+		}
+</style>
--- src/views/NOSE.vue
+++ src/views/NOSE.vue
@@ -1,33 +1,99 @@
 <template>
-  <el-container>
-    <el-main>
-      <books class="books-area"></books>
-    </el-main>
-    <el-col :span="16">
-          <el-button type="primary" icon="el-icon-plus" size="small" circle @click="plus(i)"></el-button> 
-    </el-col>
-  </el-container>
-  
+  <div class="demo-image__lazy">
+  <div class="block" v-for="url in urls" :key='url'>
+    <el-row :gutter="20">
+      <el-col>
+        <el-image
+        style="width:500px;height:500px"
+        :src='url'>
+        </el-image>
+      </el-col>
+
+      <el-col>
+        <el-button @click="deletepicture(url)" type="primary" icon="el-icon-delete">删除</el-button>
+      </el-col>
+    </el-row>
+  </div>
+  <!-- <el-col :span="16">
+      <el-button type="primary" icon="el-icon-plus" size="small" circle @click="callFile()"></el-button> 
+  </el-col> -->
+  <el-upload
+    class="upload-demo"
+    ref="upload"
+    action="https://jsonplaceholder.typicode.com/posts/"
+    :on-preview="handlePreview"
+    :on-remove="handleRemove"
+    :file-list="urls"
+    :auto-upload="false">
+    <el-button slot="trigger" size="small" type="primary">选取文件</el-button>
+    <el-button style="margin-left: 10px;" size="small" type="success" @click="submitUpload">上传到服务器</el-button>
+    <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
+  </el-upload>
+</div>
 </template>
 
 <script>
-  import Books from './Picture'
   export default {
-    name: 'AppLibrary',
-    components: {Books},
-    mounted:{
-      plus (index) {
-        this.list.push({})
+    data() {
+      return {
+        urls: [
+          'https://fuss10.elemecdn.com/a/3f/3302e58f9a181d2509f3dc0fa68b0jpeg.jpeg',
+          'https://fuss10.elemecdn.com/1/34/19aa98b1fcb2781c4fba33d850549jpeg.jpeg',
+          'https://fuss10.elemecdn.com/0/6f/e35ff375812e6b0020b6b4e8f9583jpeg.jpeg',
+          'https://fuss10.elemecdn.com/9/bb/e27858e973f5d7d3904835f46abbdjpeg.jpeg',
+        ]
+      }
+    },
+    methods:{
+      deletepicture(row){
+         console.log(row);
+         if(this.$store.state.user_role !== 'admin'){
+          alert("无更改权限，仅管理员可操作");
+        }
+        const _this = this
+        axios.delete('' + row)
+          .then(res => {
+            if (res.data.status === "success")
+              _this.$alert(row+'删除成功','消息',{
+                confirmButtonText: '确定',
+                callback: action => {
+                  window.location.reload()
+                  //动态刷新
+                }
+              })
+        })
+        .catch(error => {
+          console.error(error);
+        })
       },
-    }
+      callFile () {
+        //点击添加图片按钮，触发type:"file"的input标签
+        let fileDom = document.querySelector("#file")
+        fileDom.click()
+      },
+      submitUpload() {
+        this.$refs.upload.submit();
+      },
+      handleRemove(file, fileList) {
+        console.log(file, fileList);
+      },
+      handlePreview(file) {
+        console.log(file);
+      }
+    },
+    mounted() {
+      const that = this
+      axios.get('')
+        .then(res => {
+          if (res.data.status === "success")
+          {
+            that.urls = res.data.data_list
+            // console.log(that.tableData);
+          }
+        })
+        .catch(error => {
+          console.error(error)
+        })
+    },
   }
 </script>
-
-<style scoped>
-  .books-area {
-    width: 990px;
-    margin-left: auto;
-    margin-right: auto;
-  }
-</style>
-
